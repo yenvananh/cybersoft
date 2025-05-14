9da2546fd7976f3a9bc1b3b13e442218bf671bcb
yenvananh <yenvananh@gmail.com>
2025-05-14 12:52:41 +0700

    Update demo.dart

--- demo.dart
+++ demo.dart
@@ -1,2 +1,10 @@
 // đây là comment
-// nội dung thêm vào
+// Các lệnh git
+// git add . => Xác minh các file thay đổi
+// git commit -m "[ghi chú]" => Lưu vào Git
+// git push => Đẩy dữ liệu lên GitHub
+// git pull => Lấy dữ liệu từ GitHub
+// git branch => Xem tất cả các nhánh
+// git branch [tên nhánh] => Tạo nhánh làm việc [tên nhánh] - tên người code
+// git checkout [tên nhánh] => Chuyển sang nhánh [tên nhánh]
+// git merge => Ghép nội dung 2 nhánh. Lưu ý: Không đứng tại nhánh con để merge
